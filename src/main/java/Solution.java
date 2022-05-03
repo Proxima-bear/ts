ace397ee8e8ee277cc0dca71a4b41a372eb3b689
Андрей <medvedev32134@gmail.com>
2022-05-03 16:03:51 +0300

    english

---
+++ src/main/java/Solution.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Solution {
+
+
+
+
+    public static void main(String[] args)  {
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+        int b = in.nextInt();
+        if (a>0 && b>0 || a<0 && b<0)
+            System.out.println("Один и тот же знак");
+        else if (a==0 || b==0)
+            System.out.println("zero");
+        else
+            System.out.println("разные знаки");
+
+        Math.
+    }
+}
